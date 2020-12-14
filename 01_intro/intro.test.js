ec1a257ca04b6d49651a10271761cebe413c1bd6
Evgenii Markov <emarkov@emarkov-mn1.linkedin.biz>
2020-12-14 11:53:10 -0800

    added new conditions with greaterThan and LessThan

--- 01_intro/intro.test.js
+++ 01_intro/intro.test.js
@@ -4,3 +4,10 @@ test('Sum should be sum of two values', () => {
   expect(sum(1, 3)).toBe(4);
   expect(sum(2, 3)).toBe(5);
 });
+
+test('Should return proper values in compare to other numbers', () => {
+  expect(sum(1, 3)).toBeGreaterThan(2);
+  expect(sum(2, 3)).toBeGreaterThanOrEqual(5);
+  expect(sum(2, 3)).toBeLessThanOrEqual(5);
+  expect(sum(2, 3)).toBeLessThan(7);
+});
